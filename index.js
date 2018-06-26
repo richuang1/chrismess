d6ae94fffa72e47565f4b45c790ae080368ba904
richuang1 <richuang@iu.edu>
2018-06-26 11:07:25 -0400

    Finished Homework

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-console.log("It Works!")
-console.warn("Omar coming!")
-console.error("Aw, snap")
-const button = document.querySelector("button")
+
+const button = document.querySelector('button')
+const button2=document.querySelector('#submitButton')
+const enter=keyCode=13
+const updateText=function(){
+    const p =document.querySelector('.oldestText')
+    p.textContent = 'This is the updated, newer text.'
+}
+const updateInput=function(){
+    const g=document.querySelector('h1')
+    g.textContent=document.querySelector("#inputText").value
+    event.preventDefault()
+}
+button.addEventListener('click',updateText)
+button2.addEventListener('click',updateInput)
+enter.addEventListener('keyup')
