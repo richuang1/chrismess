e8a3a746818490cc2385f4c6a0f89c350ba497cd
richuang1 <richuang@iu.edu>
2018-06-28 10:03:44 -0400

    Homework 3

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const form = document.querySelector('form#flickForm')
-const updateList = function(ev) {
-  console.log(ev.target)
-  
-  ev.preventDefault()
-  const f = ev.target
+class App {
+  constructor() {
+    this.flicks=[]
+    const form = document.querySelector('form#flickForm')
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault()
+      this.handleSubmit(ev)
+    })
+  }
 
-  const flickName = f.flickName.value
-  const item= document.createElement("li")
-  item.textContent=flickName
-  const list = document.querySelector('#flicks')
-  list.appendChild(item)
+  renderProperty(name, value) {
+    const span = document.createElement('span')
+    span.classList.add(name)
+    span.textContent = value
+    return span
 
+  }
 
-  console.log(f.flickYear)
-  const flickYear = f.flickYear.value
-  const year =document.createElement("li")
+  renderItem(flick) {
+    const item = document.createElement('li')
+    item.classList.add('flick')
 
-  year.textContent=flickYear
-  list.appendChild(year)
-  f.reset()
+    // get the list of properties
+    const properties = Object.keys(flick)
+
+    // loop over each property
+    properties.forEach((propertyName) => {
+      // build a span
+      const span = this.renderProperty(propertyName, flick[propertyName])
+      item.appendChild(span)
+    })
+
+    return item
+    
+  }
+  //var btn=document.createElement("BUTTON");
+  //var e=document.createTextNode("Remove");
+  //btn.appendChild(e);
+  //document.body.appendChild(btn);
+  handleSubmit(ev) {
+    const f = ev.target
+
+    const flick = {
+      name: f.flickName.value,
+      chris: f.chrisName.value,
+    }
+    this.flicks.push(flick)
+    const item = this.renderItem(flick)
+
+    const list = document.querySelector('#flicks')
+    list.appendChild(item)
+
+    f.reset()
+    f.flickName.focus()
+  }
 }
 
-form.addEventListener('submit', updateList)
+const app = new App()
